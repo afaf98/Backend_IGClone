cdaa3963a19d9c8abd58926d619c96c06fcdc576
afaf98 <afaf.ibrahimi@gmail.com>
2020-12-31 12:19:19 +0100

    Defining and array for images

--- routes/images.js
+++ routes/images.js
@@ -4,13 +4,13 @@ const { Router } = express;
 
 const router = new Router();
 
-router.get("/images", (request, response) =>
-  response.send([
-    { id: 1, url: "https://unsplash.com/ksjdvksdfg" },
-    { id: 2, url: "https://unsplash.com/ksjdvksdfg" },
-    { id: 3, url: "https://unsplash.com/ksjdvksdfg" },
-    { id: 4, url: "https://unsplash.com/ksjdvksdfg" },
-  ])
-);
+const images = [
+  { id: 1, url: "https://unsplash.com/ksjdvksdfg" },
+  { id: 2, url: "https://unsplash.com/ksjdvksdfg" },
+  { id: 3, url: "https://unsplash.com/ksjdvksdfg" },
+  { id: 4, url: "https://unsplash.com/ksjdvksdfg" },
+];
+
+router.get("/images", (request, response) => response.send(images));
 
 module.exports = router;
