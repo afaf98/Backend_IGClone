455e3e47ea35fda761e6d649d133b7eadb9b2872
afaf98 <afaf.ibrahimi@gmail.com>
2021-02-15 14:34:12 +0100

    Add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const PORT = require("./constant");
 const app = express();
+const cors = require("cors")
 
 const testRoute = require("./testRoute.js");
 
 app.use(express.json());
+app.use(cors)
 
 app.use(testRoute)
 
