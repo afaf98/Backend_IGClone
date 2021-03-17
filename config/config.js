0ed8075a5f0832dc95d30dcac2d611304d08736d
afaf98 <afaf.ibrahimi@gmail.com>
2021-03-17 10:16:25 +0100

    Setup production database

--- config/config.js
+++ config/config.js
@@ -11,15 +11,7 @@ module.exports = {
     logging: false,
   },
   production: {
-<<<<<<< HEAD
     use_env_variable: "PRODUCTION_DATABASE",
     dialect: "postgres",
-=======
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
->>>>>>> edf62289401a9f850c4bd55dea3099affec39898
   },
 };
