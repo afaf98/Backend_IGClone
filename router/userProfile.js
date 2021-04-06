cd22e44fbbfd16e9b9a865f6070f0aecc9d45f5a
afaf98 <afaf.ibrahimi@gmail.com>
2021-04-06 14:00:02 +0200

    Send profile image

--- router/userProfile.js
+++ router/userProfile.js
@@ -8,8 +8,12 @@ const router = new Router();
 
 router.get("/profile", authMiddleware, async (req, res) => {
   try {
-    const images = await Image.findAll({ where: { userId: req.user.id } });
+    const images = await Image.findAll({
+      where: { userId: req.user.id },
+      order: [["createdAt", "DESC"]],
+    });
     return res.status(200).json({
+      profileImage: images.length === 0 ? [] : images[0].url,
       images: images,
       user: { name: req.user.firstName, lastName: req.user.lastName },
     });
